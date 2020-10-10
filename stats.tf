3d477d881cbb35f00e291381e5c0c7be2332f4ce
Xander Flood <xander.t.flood@gmail.com>
2020-10-09 21:48:54 -0400

    remove helm release for now

--- stats.tf
+++ stats.tf
@@ -8,38 +8,38 @@ resource "kubernetes_namespace" "stats" {
 # helm upgrade --namespace stats --install loki loki/loki-stack \
 # --set fluent-bit.enabled=true,promtail.enabled=false,grafana.enabled=true,prometheus.enabled=true,prometheus.alertmanager.persistentVolume.enabled=false,prometheus.server.persistentVolume.enabled=false
 
-resource "helm_release" "loki" {
-  name       = "loki"
-  namespace  = kubernetes_namespace.stats.metadata.0.name
-  repository = "https://grafana.github.io/loki/charts"
-  chart      = "loki/loki-stack"
-  # version    = "0.41.1"
+# resource "helm_release" "loki" {
+#   name       = "loki"
+#   namespace  = kubernetes_namespace.stats.metadata.0.name
+#   repository = "https://grafana.github.io/loki/charts"
+#   chart      = "loki/loki-stack"
+#   # version    = "0.41.1"
 
-  # set {
-  #   name  = "fluent-bit.enabled"
-  #   value = true
-  # }
-  # set {
-  #   name  = "promtail.enabled"
-  #   value = false
-  # }
-  # set {
-  #   name  = "grafana.enabled"
-  #   value = true
-  # }
-  # set {
-  #   name  = "prometheus.enabled"
-  #   value = true
-  # }
-  # set {
-  #   name  = "prometheus.alertmanager.persistentVolume.enabled"
-  #   value = false
-  # }
-  # set {
-  #   name  = "prometheus.server.persistentVolume.enabled"
-  #   value = false
-  # }
-}
+#   # set {
+#   #   name  = "fluent-bit.enabled"
+#   #   value = true
+#   # }
+#   # set {
+#   #   name  = "promtail.enabled"
+#   #   value = false
+#   # }
+#   # set {
+#   #   name  = "grafana.enabled"
+#   #   value = true
+#   # }
+#   # set {
+#   #   name  = "prometheus.enabled"
+#   #   value = true
+#   # }
+#   # set {
+#   #   name  = "prometheus.alertmanager.persistentVolume.enabled"
+#   #   value = false
+#   # }
+#   # set {
+#   #   name  = "prometheus.server.persistentVolume.enabled"
+#   #   value = false
+#   # }
+# }
 
 resource "kubernetes_ingress" "grafana" {
   metadata {
