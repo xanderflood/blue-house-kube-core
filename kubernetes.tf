a86ea0474701be8782ac78590a0f68c70aaed6c1
Xander Flood <xander.t.flood@gmail.com>
2020-10-09 22:41:15 -0400

    clean up provider config

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,23 +1,11 @@
-# provider "helm" {
-#   debug = true
-#   kubernetes {
-#     config_context = "blue-house"
-#   }
-# }
 provider "kubernetes" {
   load_config_file = false
-  # config_context = "blue-house"
 }
-provider "kubernetes-alpha" {
-  # config_path    = "~/.kube/config"
-  # config_context = "blue-house"
-}
+provider "kubernetes-alpha" {}
 
 terraform {
   backend "kubernetes" {
-    secret_suffix    = "kube-system"
-    load_config_file = true
-    config_context   = "blue-house"
-    # in_cluster_config = true
+    secret_suffix     = "kube-system"
+    in_cluster_config = true
   }
 }
