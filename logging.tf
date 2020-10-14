9e7f6d0f13f9bdd4c89356f2a38068250e95428b
Xander Flood <xander.t.flood@gmail.com>
2020-10-13 20:50:43 -0400

    fix

--- logging.tf
+++ logging.tf
@@ -10,48 +10,60 @@ resource "kubernetes_daemonset" "logizio-fluentd" {
   }
 
   spec {
-    toleration {
-      key    = "node-role.kubernetes.io/master"
-      effect = "NoSchedule"
-    }
-
-    container {
-      image = "logzio/logzio-k8s:1.0.0"
-      name  = "fluentd"
-
-      resources {
-        limits {
-          memory = "200Mi"
-        }
-        requests {
-          cpu    = "100m"
-          memory = "200Mi"
+    template {
+      metadata {
+        labels = {
+          "k8s-app"                       = "fluentd-logzio"
+          "version"                       = "v1"
+          "kubernetes.io/cluster-service" = "true"
         }
       }
 
-      volume_mount {
-        name       = "varlog"
-        mount_path = "/var/log"
-      }
-      volume_mount {
-        name       = "varlibdockercontainers"
-        mount_path = "/var/lib/docker/containers"
-        read_only  = true
-      }
-    }
+      spec {
+        toleration {
+          key    = "node-role.kubernetes.io/master"
+          effect = "NoSchedule"
+        }
 
-    termination_grace_period_seconds = 30
+        container {
+          image = "logzio/logzio-k8s:1.0.0"
+          name  = "fluentd"
 
-    volume {
-      name = "varlog"
-      host_path {
-        path = "/var/log"
-      }
-    }
-    volume {
-      name = "varlibdockercontainers"
-      host_path {
-        path = "/var/lib/docker/containers"
+          resources {
+            limits {
+              memory = "200Mi"
+            }
+            requests {
+              cpu    = "100m"
+              memory = "200Mi"
+            }
+          }
+
+          volume_mount {
+            name       = "varlog"
+            mount_path = "/var/log"
+          }
+          volume_mount {
+            name       = "varlibdockercontainers"
+            mount_path = "/var/lib/docker/containers"
+            read_only  = true
+          }
+        }
+
+        termination_grace_period_seconds = 30
+
+        volume {
+          name = "varlog"
+          host_path {
+            path = "/var/log"
+          }
+        }
+        volume {
+          name = "varlibdockercontainers"
+          host_path {
+            path = "/var/lib/docker/containers"
+          }
+        }
       }
     }
   }
